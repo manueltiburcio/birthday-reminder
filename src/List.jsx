2b69780c57f93adc583977f501bc1d71bd323fed
Manuel Tiburcio <manuelmtiburcio@gmail.com>
2022-03-25 14:20:10 -0700

    Added handleClick

---
+++ src/List.jsx
@@ -0,0 +1,19 @@
+const List = ({ people }) => {
+  return (
+    <>
+      {people.map((person) => {
+        return (
+          <article key={person.id} className="person">
+            <img src={person.image} alt={person.name} />
+            <div>
+              <h4>{person.name}</h4>
+              <p>{person.age} years old</p>
+            </div>
+          </article>
+        );
+      })}
+    </>
+  );
+};
+
+export default List;
